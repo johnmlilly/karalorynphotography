d5a431252d02a6d45f7b3dc52e8d728612961736
John Lilly <jm2lilly@gmail.com>
2025-02-14 08:25:04 -0500

    Update image plugin

--- eleventy.config.js
+++ eleventy.config.js
@@ -5,16 +5,40 @@ import { eleventyImageTransformPlugin } from "@11ty/eleventy-img";
 
 export default function(eleventyConfig) {
 
-eleventyConfig.addPassthroughCopy('./src/styles');
-eleventyConfig.addPassthroughCopy('./src/assets') ;
-eleventyConfig.addPassthroughCopy('./src/scripts');
-eleventyConfig.addPassthroughCopy('./src/admin');
-eleventyConfig.addPlugin(emojiReadTime, {
-  emoji: "🕒",
-  label: "minute read"
-});
-eleventyConfig.addPlugin(metagen);
-eleventyConfig.addPlugin(eleventyImageTransformPlugin);
+  eleventyConfig.addPassthroughCopy('./src/styles');
+  eleventyConfig.addPassthroughCopy('./src/assets') ;
+  eleventyConfig.addPassthroughCopy('./src/scripts');
+  eleventyConfig.addPassthroughCopy('./src/admin');
+
+  // Meta data plugin for SEO
+  eleventyConfig.addPlugin(metagen);
+
+  // Reading time for blog posts
+  eleventyConfig.addPlugin(emojiReadTime, {
+    emoji: "🕒",
+    label: "minute read"
+  });
+
+	// Image optimization: https://www.11ty.dev/docs/plugins/image/#eleventy-transform
+	eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
+		// Output formats for each image.
+		formats: ["avif", "webp", "auto"],
+
+		// widths: ["auto"],
+
+		failOnError: false,
+		htmlOptions: {
+			imgAttributes: {
+				// e.g. <img loading decoding> assigned on the HTML tag will override these values.
+				loading: "lazy",
+				decoding: "async",
+			}
+		},
+
+		sharpOptions: {
+			animated: true,
+		},
+	});
 
 // Get current year for footer
 eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
